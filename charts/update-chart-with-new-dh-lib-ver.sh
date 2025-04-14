1a86aa463920e1b25334bb57ee17d693c7851d66
Test-Automation-Engineer <menno.slingerland@denhaag.nl>
2025-04-14 20:14:33 +0200

    Update charts

--- charts/update-chart-with-new-dh-lib-ver.sh
+++ charts/update-chart-with-new-dh-lib-ver.sh
@@ -1,5 +1,5 @@
-currentVal=0.1.11
-newVal=0.1.14
+currentVal=0.1.14
+newVal=0.1.15
 file=Chart.yaml
 
 find ./ -iname "$file" -exec sed -i "$(yq '.dependencies.[] | select(.name == "dh-lib").version | line' '{}')s/$currentVal/$newVal/" '{}' ';'
